3f22eb0ab2ee2bb1cc4ac418ceb24f6fec224481
Saeed Aliyan <saeed0aliyan@gmail.com>
2023-04-22 22:33:18 +0330

    feat: handle start again status

--- Rock-paper-scissors.py
+++ Rock-paper-scissors.py
@@ -1,6 +1,5 @@
 import random
 
-#at the first set scores of players 
 
 
 
@@ -10,14 +9,19 @@ game_role = {
     "Paper" : "rock", #Paper is win 
     "scissord" : "rock" #scissord is win 
 }
-computer_score_coefficient = 1
-user_score_coefficient = 1
+computer_score_coefficient = 0
+user_score_coefficient = 0
 
 
+#at the first set 0 to scores of players 
 computer_score = 0
 user_score = 0
 
+# append winer of each turn to win_history for record 
 win_history=[]
+
+finish=False
+
 
 # the while loop for sequential execution
 while True:
@@ -42,37 +46,55 @@ pleae write your choice : """)
 
     elif computer_choice is game_role[user_choice]:
         print("you win !")
-        if user_score != 0 :
-            user_score *= user_score_coefficient
-        else:
-            user_score =1
+        user_score = user_score  + (2 ** user_score_coefficient)
         user_score_coefficient += 1
-        computer_score_coefficient = 1
+        
+        #set 0 to coefficient for calculate
+        #without coefficient in another user next turn 
+        computer_score_coefficient = 0
         win_history.append("user")
 
     
     else :
         print("you lose !")
-        if computer_score != 0:
-            computer_score *= computer_score_coefficient
-        else :
-            computer_score =1
-        computer_score_coefficient += 1
-        user_score_coefficient = 1
+        computer_score = computer_score + (2 ** computer_score_coefficient)
+        user_score_coefficient = 0
         win_history.append("computer")
         
 
-    
+    # show game detail in 
     print("your score : " + str(user_score))
     print("computer score : " + str(computer_score))
-    print(win_history)
+    print('win history : ' , win_history)
+
+
     #check winner and break the loop
-    #winner who is have over 50 point 
-    if computer_score >= 50 or computer_score_coefficient == 6:
+    #winner who is have over 50 point or win 5 time 
+    if computer_score >= 2 or computer_score_coefficient == 5:
         print("computer Win !!!!")
-        break
-    if user_score >= 50 or user_score_coefficient == 6:
+        finish=True
+    if user_score >= 2 or user_score_coefficient == 5:
         print('user Win *********!')
-        break
-    
+        finish=True
+
+
+
+    if finish :
+
+        start_again = input("do you want play again (t/f) ?")
+
+        if start_again == "t":
+            computer_score_coefficient = 0
+            user_score_coefficient = 0
+            computer_score = 0
+            user_score = 0
+            win_history.clear()
+            finish=False
+            print("\n game is startind ... \n \n")
+
+        else:
+            print('---------')
+            print("game is finished !")
+            break
+
     print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
